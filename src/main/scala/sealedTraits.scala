3a00e68d134abea5732eeb854a2fd97c8315b1c8
davidsan <david.sanluis@qrsof.com>
2023-05-15 11:42:33 -0600

    sealedTraits

---
+++ src/main/scala/sealedTraits.scala
@@ -0,0 +1,20 @@
+object sealedTraits extends App {
+
+  sealed trait Forma
+
+  case class Rectangulo(altura: Int, ancho: Int) extends Forma
+  case class Circulo(radio: Int) extends Forma
+
+
+  // val matchForma: Forma => Double =
+  def matchForma(forma : Forma) = forma match
+    case Rectangulo(altura, ancho) => altura * ancho
+    case Circulo(radio) => Math.PI * radio * radio
+  end matchForma
+
+  val circulo = Circulo(4)
+  val rectangulo = Rectangulo(5, 10)
+  val encontrarForma = matchForma(circulo)
+  println(encontrarForma)
+
+}
